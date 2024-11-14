11813d86c30f2f92b0ecc7101443d06dd2c44e91
dev.arthur <contato.arthurreis222@gmail.com>
2024-11-14 20:45:04 -0300

    feat: insert money input

--- src/app/sign/(login)/page.tsx
+++ src/app/sign/(login)/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import FormLogin from "./sessions/form-login";
+
+export const metadata: Metadata = {
+  title: "Login - Painel Admin",
+};
 
 const Login = () => {
   return <FormLogin />;
--- src/app/sign/register/page.tsx
+++ src/app/sign/register/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import FormRegister from "./sessions/form-register";
+
+export const metadata: Metadata = {
+  title: "Cadastro - Painel Admin",
+};
 
 const Register = () => {
   return <FormRegister />;
